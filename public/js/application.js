767dc399983f8811c5a62581110c1fba56f11b2e
Travis Buckley <email": null>
2016-12-02 12:06:45 -0600

    change background, fix disply temps

--- public/js/application.js
+++ public/js/application.js
@@ -4,11 +4,14 @@ $(document).ready(function() {
     var that = $(this);
     var city = $(this).find("input[name=city]").val();
     var state = $(this).find("input[name=state]").val();
+    var getId = function(){
+      return that.attr("id");
+    }
 
     $.get("http://api.wunderground.com/api/0f9d8611536a96e6/conditions/q/"+state+"/"+city+".json")
       .done(function(response){
-        $(".temperature").append(response.current_observation.feelslike_f);
-        $('.temperature').trigger('reset')
+        var item = getId();
+        $("#city-show-form-"+item).append(response.current_observation.feelslike_f);
         // $(that).closest(".show-city").removeClass('show-city');
         // $(that).closest(".hello").addClass('display-city');
 
